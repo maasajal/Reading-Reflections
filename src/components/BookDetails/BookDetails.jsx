89376315ae8c9c43f8ccc0f46ab6aaa8a735fdfa
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-03-26 12:58:03 +0200

    Book details page implement

---
+++ src/components/BookDetails/BookDetails.jsx
@@ -0,0 +1,69 @@
+import { useLoaderData, useParams } from "react-router-dom";
+
+const BookDetails = () => {
+  const books = useLoaderData();
+  const { bookId } = useParams();
+  const idInt = parseInt(bookId);
+  const book = books.find((book) => book.bookId === idInt);
+  const {
+    image,
+    bookName,
+    author,
+    category,
+    review,
+    tags,
+    totalPages,
+    publisher,
+    yearOfPublishing,
+    rating,
+  } = book;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 mt-12 mb-32">
+      <div className="bg-[#1313130D] rounded-xl p-12">
+        <img src="../images/banner_book.png" alt="" />
+      </div>
+      <div>
+        <h2 className="text-4xl font-bold">{bookName} </h2>
+        <p className="py-4">By: {author}</p>
+        <p className="py-3 border-t border-b">{category}</p>
+        <p className="py-6 leading-8">
+          <strong>Review: </strong> {review}{" "}
+        </p>
+        <div className="flex flex-wrap gap-4 my-4 items-center">
+          <strong>Tag </strong>
+          {tags.map((tag) => (
+            <p className="text-[#23BE0A] bg-[#23BE0A0D] rounded-xl py-1 px-2 text-center">
+              {tag}
+            </p>
+          ))}
+        </div>
+        <hr />
+        <div>
+          <div className="grid grid-cols-2 my-3">
+            <p>Number of Pages: </p>
+            <p>{totalPages} </p>
+          </div>
+          <div className="grid grid-cols-2 my-3">
+            <p>Publisher: </p>
+            <p>{publisher} </p>
+          </div>
+          <div className="grid grid-cols-2 my-3">
+            <p>Year of Publishing: </p>
+            <p>{yearOfPublishing} </p>
+          </div>
+          <div className="grid grid-cols-2 my-3">
+            <p>Rating: </p>
+            <p>{rating} </p>
+          </div>
+        </div>
+        <div>
+          <button className="btn border-[#1313134D] bg-transparent px-7 mr-4 hover:bg-[#23BE0A] hover:text-white">
+            Read
+          </button>
+          <button className="btn bg-[#50B1C9] text-white px-7 hover:bg-[#23BE0A]">Wishlist</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default BookDetails;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Root from "./components/Root/Root";
 import Home from "./components/Home/Home";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
 import ListedBooks from "./components/ListedBooks/ListedBooks";
+import BookDetails from "./components/BookDetails/BookDetails";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -19,6 +20,11 @@ const router = createBrowserRouter([
       {
         path: "/listed-books",
         element: <ListedBooks />,
+      },
+      {
+        path: "/book/:bookId",
+        element: <BookDetails />,
+        loader: () => fetch("../books.json"),
       },
     ],
   },
